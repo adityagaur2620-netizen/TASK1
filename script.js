67504430998df3dc3ff124f23488235e7e419d77
adityagaur2620-netizen <aditygaur2620@gmail.com>
2025-09-13 23:55:35 +0530

    13-09

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  });
+
+
+function showMessage() {
+  alert("✅ Signed up Successfully ");
+  return false;
+}
